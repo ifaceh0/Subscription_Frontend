eacf6471fbd0908193e81d720682ff8b956abfb6
Aryaa-7 <aryanojha07@gmail.com>
2025-07-26 21:00:04 +0530

    Conversion from package to atndalone app

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   build: {
-    lib: {
-      entry: './src/index.js', // or wherever your main component is
-      name: 'GlobalSubscription',
-      fileName: 'global-subscription',
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-router-dom': 'ReactRouterDOM',
-        },
-      },
-    },
+  build: {
+    outDir: 'dist', // Output directory for Vercel
   },
 });
-
