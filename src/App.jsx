e0a04db85fa55dafb7645470be2e3257ed8b3bde
sachin <sachin@gmail.com>
2025-08-02 01:57:17 +0530

    add admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Subscription from './components/Subscription';
 import PlanDetails from './components/PlanDetails'; // You need to create this
 import Success from './components/Success'; 
 import Cancel from './components/Cancel';
+import AdminLookup from './Admin/adminLookup'; // Ensure this path is correct
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Subscription />} />
-      <Route path="/plan/:planType" element={<PlanDetails />} />
-      <Route path="/success" element={<Success />} />
-      <Route path="/cancel" element={<Cancel />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Subscription />} />
+        <Route path="/admin" element={<AdminLookup />} />
+        <Route path="/plan/:planType" element={<PlanDetails />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
   );
 }
 
